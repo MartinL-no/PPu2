bbf4c08bc39691e9bd8cb5acb459dc1cae6b2139
LarsHaugberg <110969311+LarsHaugberg@users.noreply.github.com>
2023-03-23 11:29:12 +0100

    torsdag

---
+++ TrePaRadMartinLars/Board.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace TrePaRadMartinLars
+{
+    internal class Board
+    {
+        int[] CurrentBoard;
+
+        public Board()
+        {
+            CurrentBoard = new int[9];
+        }
+        static void ShowBoard()
+        {
+            /*
+                0 1 2
+                3 4 5
+                6 7 8
+            */
+            Console.WriteLine("Use the nummber index for position");
+            Console.WriteLine("012");
+            Console.WriteLine("345");
+            Console.WriteLine("678");
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (board[i] == 0)
+                {
+                    Console.Write("_");
+                }
+
+                if (board[i] == 1)
+                {
+                    Console.Write("X");
+                }
+
+                if (board[i] == 2)
+                {
+                    Console.Write("O");
+                }
+
+                if (i == 2 || i == 5 i || i == 8) { Console.WriteLine(); }
+            }
+        }
+    }
+}
--- TrePaRadMartinLars/Program.cs
+++ TrePaRadMartinLars/Program.cs
@@ -2,10 +2,32 @@
 {
     internal class Program
     {
+        private int[] board = new int[9];
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Tre Pa Rad!");
-            Console.WriteLine("This is martins console");
+            Console.WriteLine("Welcome to Tic Tac Toe");
+            board = new Board();
+            board.ShowBoard();
+
+
+            //Run();
+            // setup a board
+            // Create board class
+
+            // ShowBoard()
+            // AddUserChoice()
+            // setup two players
+            // console output for start of game
+            // take user input from console
+            // function taking turns
+            // function for deciding/checking winner
+            // output who has won on console            
+        }
+        static void Run()
+        {
+            board = new Board();
+            board.ShowBoard();
         }
     }
 }
